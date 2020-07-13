23b19e6a13b7d6eeb3dc9c1f2d2ea4ae8096b989
Russ Ferriday <russ.ferriday@gmail.com>
2020-07-13 11:32:11 +0100

    Tidy sms monster

--- sms_monster.py
+++ sms_monster.py
@@ -10,35 +10,19 @@ def log(msg):
 
 class SmsMonster:
     def __init__(self, url):
-        self.connection = self.get_connection(url)
-        self.client = None
-        self.sm = None
-
-    def get_connection(self, url):
-        log('getting connection')
-        con = AuthorizedConnection(url)
-        log('got connected')
-        return con
-
-    def get_client(self, con):
-        log('getting client')
-        cli = Client(con)
-        log('got client')
-        return cli
+        self.connection = AuthorizedConnection(url)
+        self.client = Client(self.connection)
+        self.sm = self.client.sms
 
     def simple_send(self, number, msg):
-        if self.client is None:
-            self.client = self.get_client(self.connection)
-            self.sm = self.client.sms
         self.sm.send_sms([number], msg)
         log('sent: %s' % msg)
 
     def get_next_message_content(self):
-        if self.client is None:
-            self.client = self.get_client(self.connection)
-            self.sm = self.client.sms
         ll = self.sm.get_sms_list()
         """ Notes
+        # No messages
+        {'Count': '0', 'Messages': None}
         # get_sms_list when there is 1 message...
         {'Count': '1', 'Messages': {'Message': {'Smstat': '0', 'Index': '40106', 'Phone': '+447429518822', 'Content': 'G', 
                                                'Date': '2020-07-07 19:26:02', 'Sca': None, 'SaveType': '0', 'Priority': '0', 'SmsType': '1'}
@@ -55,16 +39,16 @@ class SmsMonster:
             msg = ll['Messages']['Message'][-1]
         else:
             msg = ll['Messages']['Message']
-        content = msg['Content']
+        content = msg['Content'] or ''
         index = msg['Index']
         self.sm.delete_sms(index)
         return content
 
 
 if __name__ == '__main__':
-    NUMBER = "xx"
+    NUMBER = "put your 10 digit test target phone here..."
 
-    mon = SmsMonster(url="yy")
+    mon = SmsMonster(url="put the network address of your huaweii here")
     for i in range(10):
         while True:
             m = mon.get_next_message_content()
