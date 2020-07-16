b50f42239425099a359cadf6db0ddb07f49e78ef
Russ Ferriday <topiaruss@gmail.com>
2020-07-16 22:50:06 +0100

    update script for net addresses

--- run_xavier_detect_four.sh
+++ run_xavier_detect_four.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 set -ex
+CAM_IP=192.168.2.100
+CAM_NAME=Cam04
+#sudo docker run -it --rm \
+#  --runtime nvidia \
+#  --network host \
+#  --mount src=$(pwd)/weights,target=/usr/src/app/weights/,type=bind \
+#  --mount src=$(pwd)/inference,target=/usr/src/app/inference/,type=bind \
+#  yolov5-app:latest  ping -c 2 192.168.0.32
+ping -c 2 ${CAM_IP}
 
-sudo docker run -it --rm \
-  --runtime nvidia \
-  --network host \
-  --mount src=$(pwd)/weights,target=/usr/src/app/weights/,type=bind \
-  --mount src=$(pwd)/inference,target=/usr/src/app/inference/,type=bind \
-  yolov5-app:latest  ping -c 2 192.168.0.32
-
-
-sudo docker run -it  \
+sudo docker run   \
+  -d \
+  --restart=always \
   --runtime nvidia \
   --network host \
   --mount src=$(pwd)/weights,target=/usr/src/app/weights/,type=bind \
   --mount src=$(pwd)/inference,target=/usr/src/app/inference/,type=bind \
   yolov5-app:latest python3 detect.py \
-    --source 'rtsp://admin:onion*skin@192.168.0.32:554//h264Preview_01_sub' \
+    --source "rtsp://admin:@${CAM_IP}:554//h264Preview_01_sub" \
     --sms '+447429518822' \
-    --sms-url 'http://admin:mary-douglas-01@192.168.8.1/' 
+    --sms-url 'http://admin:mary-douglas-01@192.168.8.1/' \
+    --source-name ${CAM_NAME}
 
 
-  #--restart=always \
